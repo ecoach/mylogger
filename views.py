2f4e939bdeed46a36e77f10404912979202cc08b
jaredtritz <jtritz@umich.edu>
2013-08-27 02:28:08 -0400

    testing_new_logging

--- views.py
+++ views.py
@@ -4,16 +4,24 @@ from .models import *
 # Create your views here.
 def what_log_view(request):
     import datetime
-    try:
-        rwhat = request.GET['logEvent'] 
-    except:
-        rwhat = 'null'
-    rwho = request.user.username
     rwhen = datetime.datetime.now()
-
-    log = ELog(who=request.user, mwhen=rwhen, what=rwhat)
+    try:
+        # expect all of these to be defined by logging wrapper
+        url = request.GET['url'] 
+        category = request.GET['category'] 
+        action = request.GET['action'] 
+        label = request.GET['label'] 
+        value = request.GET['value'] 
+        json = request.GET['json'] 
+    except:
+        url = 'null' 
+        category = 'null' 
+        action = 'null' 
+        label = 'null' 
+        value = -1
+        json = 'null' 
+    log = ELog(who=request.user, mwhen=rwhen, url=url, category=category, action=action, label=label, value=value, json=json)
     log.save()
-
     return HttpResponse("Event record made.")
  
 
