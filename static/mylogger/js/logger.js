0c2041119c53d0cb59cf5206d9fe064a8ca4a4eb
jaredtritz <jtritz@umich.edu>
2013-09-03 20:20:31 -0400

    fix logging error on loading the google analytics domain bit to early

--- static/mylogger/js/logger.js
+++ static/mylogger/js/logger.js
@@ -4,11 +4,12 @@
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-
-if(CONF.production()) 
-    ga('create', 'UA-37331371-4', 'umich.edu');
-else
-    ga('create', 'UA-37331371-3', 'umich.edu');
+jQuery(document).ready(function($){
+    if(CONF.production()) 
+        ga('create', 'UA-37331371-4', 'umich.edu');
+    else
+        ga('create', 'UA-37331371-3', 'umich.edu');
+});
 
 // handles pushing log events both to our internal logging api and google analytics
 logger = {
